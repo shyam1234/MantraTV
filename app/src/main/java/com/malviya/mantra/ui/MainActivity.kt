c6f7f3b888cba20b89088b5c4bb55121176f3eff
malviya <malviya.software@gmail.com>
2024-11-10 16:27:19 +0530

    version 1.0.11

--- app/src/main/java/com/malviya/mantra/ui/MainActivity.kt
+++ app/src/main/java/com/malviya/mantra/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.viewModels
 import androidx.compose.ui.res.stringResource
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.ktx.Firebase
 import com.malviya.mantra.R
 import com.malviya.mantra.screen.GreetingScreen
@@ -18,16 +19,22 @@ class MainActivity : ComponentActivity() {
     private lateinit var analytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Obtain the FirebaseAnalytics instance.
-        analytics = Firebase.analytics
-        Timber.plant(Timber.DebugTree())
-
+        init()
         //enableEdgeToEdge()
         setContent {
             MantraTheme {
                 GreetingScreen( name = stringResource(R.string.greeting),chantViewModel)
             }
         }
+    }
+
+    private fun init() {
+        Timber.plant(Timber.DebugTree())
+        // Obtain the FirebaseAnalytics instance.
+        analytics = Firebase.analytics
+        // Initialize Firebase Crashlytics
+        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = true
+
     }
 }
 
