4178f69c8d32869e10c0fc0bf50d34d51597e53e
malviya <malviya.software@gmail.com>
2025-09-20 23:32:02 +0530

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.malviya.mantra"
         minSdk = 24
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.0.23"
+        versionCode = 24
+        versionName = "1.0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
